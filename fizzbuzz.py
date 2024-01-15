7af5cdd9aa1cef03bde4af883ec8279e64b4d285
kimjiwan-svg <uridle5@naver.com>
2024-01-15 14:11:36 +0900

    feat: Find Integer Number if it's mul of 3 or 5 or both or not

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,13 @@
 def fizz_buzz(num):
     if num % 15 == 0:
         return 'fizzbuzz'
+
+    elif num % 3 == 0:
+        return 'fizz'
+
+    elif num % 5 == 0:
+        return 'buzz'
+
     else:
         return f'{num}'
 
